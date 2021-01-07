7dbad96fedff7d29b0843e51560ab8db64246866
Peppers <1090712762@qq.com>
2021-01-07 16:37:41 +0800

    配置swagger使用token

--- src/test/java/com/peppers/exam/label/LabelServiceRetryTest.java
+++ src/test/java/com/peppers/exam/label/LabelServiceRetryTest.java
@@ -3,28 +3,17 @@ package com.peppers.exam.label;
 import com.peppers.exam.ExamApplication;
 import com.peppers.exam.config.RetryConfiguration;
 import com.peppers.exam.entity.label.TypesLabel;
-import com.peppers.exam.repository.label.LabelRepository;
 import com.peppers.exam.repository.label.TypesLabelJpaRepository;
-import com.peppers.exam.service.label.LabelApplicationService;
 import com.peppers.exam.service.label.TypesLabelService;
 import org.junit.Test;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.orm.ObjectOptimisticLockingFailureException;
-import org.springframework.stereotype.Repository;
 import org.springframework.test.annotation.Rollback;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.annotation.Resource;
 
 /**
  * @author peppers
